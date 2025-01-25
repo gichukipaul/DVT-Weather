a8e96ba9ab964a34a1845a35901f1421760ab1e4
GichukiPaul <gichukipaul043@gmail.com>
2025-01-25 20:53:22 +0300

    [INIT]: Initial setup

---
+++ DVT Weather/DVT_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVT_WeatherApp.swift
+//  DVT Weather
+//
+//  Created by GICHUKI on 25/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DVT_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DVT WeatherTests/DVT_WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  DVT_WeatherTests.swift
+//  DVT WeatherTests
+//
+//  Created by GICHUKI on 25/01/2025.
+//
+
+import Testing
+@testable import DVT_Weather
+
+struct DVT_WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
